94450849f82ec73f1b2be1086161a3765d5d7650
Ian Stewart <ian.stewarttime@gmail.com>
2016-01-21 09:34:29 -0500

    pairings pt. 1

--- students.rb
+++ students.rb
@@ -1,7 +1,25 @@
-students = ["John", "Mack", "Craig", "Megan", "Kristian", "Fran", "Butter", "Crissy", "Norm"]
+students = ["John", "Mack", "Megan", "Kristian", "Fran", "Butter", "Crissy", "Norm"]
 
 students.shuffle!
 
+count = 0
 
-puts students
+num_students = students.length - 1
+
+# until count equals the number of students
+until count == num_students
+	if num_students % 2 == 1
+		if count < num_students - 3
+	puts "#{students[count]} & #{students[count+1]}"
+		else
+	puts "#{students[count]} & #{students[count+1]} & #{students[count+2]}"
+			break
+		end
+	else
+	puts "#{students[count]} & #{students[count+1]}"
+	end
+	count += 2
+end
+
+# puts students
 
